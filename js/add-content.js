9e9096c9cbe3cc7ffe9c7da80ed4d778830bbb96
zongjie-mao <mao_zongjie@sina.com>
2018-06-11 01:42:16 +0800

    event monitoring
    learning event monitoring in form input

--- js/add-content.js
+++ js/add-content.js
@@ -54,3 +54,77 @@ var scriptout1 = document.getElementById('level3 textcontentscript')
 scriptout.innerHTML = msg
 scriptout1.textContent = msg
 
+function inputlenCheck(e,minilen) {
+    if (e.target.value.length < minilen) {
+        alert('input length less than'+minilen+',input again!')
+        e.target.value = ''
+    }
+    else {
+    }
+}
+
+/*var username1 = document.getElementById('username')
+//username1.onblur = inputlenCheck  
+
+username1.addEventListener('blur',function(e) {
+    inputlenCheck(e,6)
+},false)
+
+var mailname1 = document.getElementById('email')
+mailname1.addEventListener('blur',function(e) {
+    inputlenCheck(e,7)
+},false)
+*/
+
+var namecheck1 = document.getElementById('namecheck')
+namecheck1.addEventListener('focusout',function(e) {
+    inputlenCheck(e,6)
+},false)
+
+
+function showli(e) {
+    if(e.target.nodeName.toLowerCase()=='li') {
+        alert('li')
+    }
+}
+
+var li_box1 = document.getElementById('li_box')
+li_box1.addEventListener('click',showli,false)
+
+function countdownCal(full,countdown,price) {
+    var num
+    var realprice
+    if (full%price == 0) {
+        num = full/price;
+        realprice = (full-countdown)/num;
+    }
+    else {
+        num = Math.floor(full/price)+1;
+        realprice = (price*num-countdown)/num
+    }
+    return '应购数量:'+num+';<br />折后单价:'+realprice
+}
+
+var full
+var countdown
+var price
+var countdowncal1 = document.getElementById('cal')
+
+var elfull = document.getElementById('full')
+elfull.addEventListener('input',function() {
+    full = this.value;
+    countdowncal1.innerHTML = countdownCal(full,countdown,price)
+},false)
+
+var elcountdown =document.getElementById('countdown')
+elcountdown.addEventListener('input',function() {
+    countdown = this.value;
+    countdowncal1.innerHTML = countdownCal(full,countdown,price)
+},false)
+
+var elprice =document.getElementById('price')
+elprice.addEventListener('input',function() {
+    price = this.value;
+    countdowncal1.innerHTML = countdownCal(full,countdown,price)
+},false)
+
